90ad990ffbd0b0933a21a66db0564e2dfe36d7c0
Igor Assis Mendes da Silva <105676837+igorassisx@users.noreply.github.com>
2023-10-11 21:29:48 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,24 @@
+var game;
+    var gameOptions = {
+        tileSize: 200, 
+        tileSpacing: 20,  
+        boardSize : {
+            rows: 4,
+            cols: 4
+        }
+
+    }
+
 window.onload = function() {
+
     var gameConfig = {
-        width: 800,
-        height: 800, 
+        width: gameOptions.boardSize.cols * (gameOptions.tileSize + gameOptions.tileSpacing) + gameOptions.tileSpacing,
+        height: gameOptions.boardSize.rows * (gameOptions.tileSize + gameOptions.tileSpacing) + gameOptions.tileSpacing, 
         backgroundColor: 0xecf0f1,
         scene: [bootGame, playGame]
     }
+
+
     game = new Phaser.Game(gameConfig);
     window.focus();
     resizeGame();
@@ -31,9 +45,15 @@ window.onload = function() {
         create() {
             for(var i = 0; i < 4; i++) {
                 for(var j = 0; j < 4; j++) {
-                    this.add.image(100 + j * 200, 100 + i * 200, "emptytile");
+                    var tilePosition = this.getTilePosition(i, j)
+                    this.add.image(tilePosition.x, tilePosition.y, "emptytile");
                 }
             }
+        }
+        getTilePosition(row, col) {
+            var posX = gameOptions.tileSpacing * (col +  1) + gameOptions.tileSize * (col + 0.5);
+            var posY = gameOptions.tileSpacing * (row +  1) + gameOptions.tileSize * (row + 0.5);
+            return new Phaser.Geom.Point(posX, posY);
         }
     }
 
